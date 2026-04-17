671490522ed59ad4df9d12983d4b128b23364090
Zeta_Null <z0zetanull@gmail.com>
2026-04-16 22:37:58 -0400

    pre-pokemon held items oh god

--- data/maps/map_data.asm
+++ data/maps/map_data.asm
@@ -1,10 +1,10 @@
 SECTION "Maps", ROMX
 
 INCLUDE "data/maps/maps.asm"
+
+
+SECTION "Map Attributes", ROMX
+
 INCLUDE "data/maps/attributes.asm"
-
-
 INCLUDE "data/maps/blocks.asm"
-
-
 INCLUDE "data/maps/scripts.asm"
